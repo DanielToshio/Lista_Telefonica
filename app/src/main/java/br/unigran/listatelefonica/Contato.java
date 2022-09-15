c04a7c26209c11dc306f79c159628ffa4116b2f8
DanielToshio <802.261@alunos.unigran.br>
2022-09-14 22:34:34 -0300

    layout

--- app/src/main/java/br/unigran/listatelefonica/Contato.java
+++ app/src/main/java/br/unigran/listatelefonica/Contato.java
@@ -1,2 +1,34 @@
-package br.unigran.listatelefonica;public class Contato {
+package br.unigran.listatelefonica;
+
+import java.util.Date;
+
+public class Contato {
+
+    private String nome;
+    private String telefone;
+    private Date nascimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
 }
